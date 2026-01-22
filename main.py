c78a9524b6f83bb19821fa034065d9035f10c9d0
antohus-liga <antonioslotylo0904@gmail.com>
2026-01-22 17:16:31 +0000

    db creation in base repo

---
+++ main.py
@@ -0,0 +1,4 @@
+from src.repos.base import Base
+
+
+base = Base()
---
+++ src/repos/base.py
@@ -0,0 +1,57 @@
+import sqlite3
+
+
+class Base:
+    def __init__(self, db_path: str = "finance_app.db"):
+        self.db_path = db_path
+        self._create_tables()
+
+    def get_connection(self):
+        return sqlite3.connect(self.db_path)
+
+    def _create_tables(self):
+        with self.get_connection() as conn:
+            cursor = conn.cursor()
+            cursor.execute(
+                """
+                CREATE TABLE IF NOT EXISTS loans (
+                    id TEXT PRIMARY KEY,
+                    lender_name TEXT NOT NULL,
+                    total_amount TEXT NOT NULL,
+                    current_amount TEXT NOT NULL,
+                    interest_rate TEXT,
+                    start_date TEXT,
+                    status TEXT DEFAULT 'ACTIVE'
+                );
+            """
+            )
+
+            cursor.execute(
+                """
+                CREATE TABLE IF NOT EXISTS expenses (
+                    id TEXT PRIMARY KEY,
+                    amount TEXT NOT NULL,
+                    payee TEXT,
+                    category TEXT,
+                    date TEXT NOT NULL,
+                    loan_id TEXT
+                    notes TEXT,
+                    FOREIGN KEY (loan_id) REFERENCES loans (id) ON DELETE SET NULL
+                );
+            """
+            )
+
+            cursor.execute(
+                """
+                CREATE TABLE IF NOT EXISTS incomes (
+                    id TEXT PRIMARY KEY,
+                    amount TEXT NOT NULL,
+                    source TEXT,
+                    category TEXT,
+                    date TEXT NOT NULL,
+                    notes TEXT
+                );
+            """
+            )
+
+            conn.commit()
