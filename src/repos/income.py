99b564581c3d9a1b32b6502f8062b46ff541485d
antohus-liga <antonioslotylo0904@gmail.com>
2026-01-28 10:57:59 +0000

    added income repo

---
+++ src/repos/income.py
@@ -0,0 +1,24 @@
+from src.models.income import Income
+from src.repos.base import Base
+
+
+class IncomeRepo(Base):
+    def save(self, income: Income):
+        sql = """
+            INSERT INTO incomes (id, amount_original, currency_code, exchange_rate, amount_base, source, date, notes)
+            VALUES(?, ?, ?, ?, ?, ?, ?, ?)
+        """
+        with self.get_connection() as conn:
+            conn.execute(
+                sql,
+                (
+                    str(income.id),
+                    str(income.amount_original),
+                    income.currency_code.value,
+                    str(income.exchange_rate),
+                    str(income.amount_base),
+                    income.source.value,
+                    income.date,
+                    income.notes,
+                ),
+            )
